fb9a24a8f4f2bb842d19e22ffe4d69350c4abb0c
Ran Avner <ran.avner@weizmann.ac.il>
2026-05-07 00:16:39 +0300

    removed build from gitignore

---
+++ build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/DependInfo.cmake
@@ -0,0 +1,85 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_emu/dji_media_file_manage/dji_media_file_core.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_emu/dji_media_file_manage/dji_media_file_core.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_emu/dji_media_file_manage/dji_media_file_jpg.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_emu/dji_media_file_manage/dji_media_file_jpg.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_emu/dji_media_file_manage/dji_media_file_mp4.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_emu/dji_media_file_manage/dji_media_file_mp4.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_emu/test_payload_cam_emu_base.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_emu/test_payload_cam_emu_base.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_emu/test_payload_cam_emu_media.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_emu/test_payload_cam_emu_media.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_emu/test_raspberry_pi_camera.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_emu/test_raspberry_pi_camera.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/camera_manager/test_camera_manager.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/camera_manager/test_camera_manager.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/cloud_api/test_cloud_api_by_web_socket.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/cloud_api/test_cloud_api_by_web_socket.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/data_transmission/test_data_transmission.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/data_transmission/test_data_transmission.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/fc_subscription/test_fc_subscription.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/fc_subscription/test_fc_subscription.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/flight_control/test_flight_control.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/flight_control/test_flight_control.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/gimbal_emu/test_payload_gimbal_emu.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/gimbal_emu/test_payload_gimbal_emu.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/gimbal_manager/test_gimbal_manager.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/gimbal_manager/test_gimbal_manager.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/hms/test_hms.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/hms/test_hms.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/interest_point/test_interest_point.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/interest_point/test_interest_point.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/liveview/test_liveview.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/liveview/test_liveview.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/mop_channel/test_mop_channel.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/mop_channel/test_mop_channel.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/payload_collaboration/test_payload_collaboration.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/payload_collaboration/test_payload_collaboration.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/perception/test_perception.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/perception/test_perception.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/positioning/test_positioning.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/positioning/test_positioning.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/power_management/test_power_management.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/power_management/test_power_management.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/tethered_battery/test_tethered_battery.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/tethered_battery/test_tethered_battery.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/time_sync/test_time_sync.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/time_sync/test_time_sync.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/upgrade/test_upgrade.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/upgrade/test_upgrade.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/upgrade/test_upgrade_common_file_transfer.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/upgrade/test_upgrade_common_file_transfer.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/upgrade/test_upgrade_platform_opt.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/upgrade/test_upgrade_platform_opt.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/cJSON.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/cJSON.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/dji_config_manager.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/dji_config_manager.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/util_buffer.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/util_buffer.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/util_file.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/util_file.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/util_link_list.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/util_link_list.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/util_md5.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/util_md5.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/util_misc.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/util_misc.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/utils/util_time.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/utils/util_time.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/waypoint_v2/test_waypoint_v2.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/waypoint_v2/test_waypoint_v2.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/waypoint_v3/test_waypoint_v3.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/waypoint_v3/test_waypoint_v3.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/widget/file_binary_array_list_en.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/widget/file_binary_array_list_en.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/widget/test_widget.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/widget/test_widget.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/widget/test_widget_speaker.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/widget/test_widget_speaker.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/widget_interaction_test/file_binary_array_list_en.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/widget_interaction_test/file_binary_array_list_en.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/widget_interaction_test/test_widget_interaction.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/widget_interaction_test/test_widget_interaction.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/module_sample/xport/test_payload_xport.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/__/__/module_sample/xport/test_payload_xport.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/common/monitor/sys_monitor.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/common/monitor/sys_monitor.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/common/osal/osal.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/common/osal/osal.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/common/osal/osal_fs.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/common/osal/osal_fs.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/common/osal/osal_socket.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/common/osal/osal_socket.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/common/upgrade_platform_opt/upgrade_platform_opt_linux.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/__/common/upgrade_platform_opt/upgrade_platform_opt_linux.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/raspberry_pi/application/main.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/application/main.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/raspberry_pi/application/pps.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/application/pps.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/raspberry_pi/hal/hal_i2c.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/hal/hal_i2c.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/raspberry_pi/hal/hal_network.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/hal/hal_network.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/raspberry_pi/hal/hal_uart.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/hal/hal_uart.c.o"
+  "/home/rsp/Payload-SDK/samples/sample_c/platform/linux/raspberry_pi/hal/hal_usb_bulk.c" "/home/rsp/Payload-SDK/build/samples/sample_c/platform/linux/raspberry_pi/CMakeFiles/dji_sdk_demo_on_rpi.dir/hal/hal_usb_bulk.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "LIBUSB_INSTALLED"
+  "OPUS_INSTALLED"
+  "PLATFORM_ARCH_aarch64=1"
+  "SYSTEM_ARCH_LINUX"
+  "_GNU_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../samples/sample_c/platform/linux/raspberry_pi/../../../module_sample"
+  "../samples/sample_c/platform/linux/raspberry_pi/../common"
+  "../samples/sample_c/platform/linux/raspberry_pi/application"
+  "../samples/sample_c/platform/linux/raspberry_pi/../../../../../psdk_lib/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
